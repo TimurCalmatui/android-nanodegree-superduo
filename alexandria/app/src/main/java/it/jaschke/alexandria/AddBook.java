7c20c213f6b5f617eb7d352d466be6e3aecf068e
TimurCalmatui <timur.dev@gmail.com>
2015-11-17 22:17:04 +0200

    fix: crash when the book has no authors

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -172,6 +172,10 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
             ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
             ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
         }
+        else {
+            ((TextView) rootView.findViewById(R.id.authors)).setText(null);
+        }
+        
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if (Patterns.WEB_URL.matcher(imgUrl).matches()) {
             new DownloadImage((ImageView) rootView.findViewById(R.id.bookCover)).execute(imgUrl);
